287003d3172b5dc96f35e9ecc7155aadf3231987
wnvp-softwares <wnvp115@gmail.com>
2026-07-20 18:28:22 -0600

    IPV4 Force Debuggin

--- src/server/middlewares/mailer.js
+++ src/server/middlewares/mailer.js
@@ -1,5 +1,8 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
+const dns = require('dns');
+
+dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
