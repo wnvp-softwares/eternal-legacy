77f41f1685a3a6d71f092f67f29e0cb4d7dcf820
wnvp-softwares <wnvp115@gmail.com>
2026-07-20 18:30:39 -0600

    More debugging

--- src/server/middlewares/mailer.js
+++ src/server/middlewares/mailer.js
@@ -6,8 +6,8 @@ dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
