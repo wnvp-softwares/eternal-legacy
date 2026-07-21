3ac756dd56364c3b0b9f2c364a9e1fe34bf3fa3f
wnvp-softwares <wnvp115@gmail.com>
2026-07-20 18:40:24 -0600

    Brevo update

--- src/server/middlewares/mailer.js
+++ src/server/middlewares/mailer.js
@@ -1,11 +1,10 @@
 const nodemailer = require('nodemailer');
-const dns = require('dns');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
